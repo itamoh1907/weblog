a8232bcfac129c5de5ec438042b3604d9b9193a6
zardosht <zardosht.mm@gmail.com>
2024-12-19 10:35:07 +0330

    add login and logout templates

---
+++ account/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+# Create your views here.
+
+
+def user_login(request):
+    if request.user.is_authenticated == True:
+        print('User logged in')
+        return redirect('/')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+
+    return render(request,'account/login.html',{})
+
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('/')
